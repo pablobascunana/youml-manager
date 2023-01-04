6eb7f7f5b91f57be2fa75af7bbe8fcbc81206512
pbascunana <pablobascunanasaiz@gmail.com>
2023-01-04 12:15:38 +0100

    MLM-13 Remove api django app
    # Conflicts:
    #	src/api/urls.py

--- src/api/viewsets/train/view.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import viewsets, status, mixins
-from rest_framework.response import Response
-
-
-class TrainViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
-
-    @staticmethod
-    def list(request, *args, **kwargs):
-        return Response(status=status.HTTP_200_OK)
