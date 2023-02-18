e39777bc7b640224d0c2932b5ca204d3a66de531
pbascunana <pablobascunanasaiz@gmail.com>
2023-02-18 17:47:21 +0100

    Typing and add responses for unit tests

--- src/core/kubernetes/jobs_manager.py
+++ src/core/kubernetes/jobs_manager.py
@@ -3,7 +3,7 @@ import time
 from typing import Dict
 
 from kubernetes import client, config
-from kubernetes.client import BatchV1Api, V1Job
+from kubernetes.client import BatchV1Api, V1Job, V1Status
 
 logger = logging.getLogger("youML-manager")
 
@@ -12,36 +12,42 @@ class KubernetesJobManager:
 
     def __init__(self, job_name: str, container_image_name: str, params: Dict[str, str]):
         config.load_kube_config()
-        self.params = params
         self.batch_v1 = client.BatchV1Api()
         self.job = self._create_job_object(job_name, container_image_name, params)
 
     @staticmethod
     def _create_job_object(job_name: str, image_name: str, params: Dict[str, str]):
-        container = client.V1Container(name=job_name, image=image_name, image_pull_policy="Always")  # args=[params["dataset"], params["path"]]
+        container = client.V1Container(name=job_name, image=image_name,
+                                       image_pull_policy="Always")  # args=[params["dataset"], params["path"]]
         template = client.V1PodTemplateSpec(metadata=client.V1ObjectMeta(labels={'name': job_name}),
                                             spec=client.V1PodSpec(restart_policy='Never', containers=[container]))
         spec = client.V1JobSpec(template=template)
         return client.V1Job(api_version='batch/v1', kind='Job', metadata=client.V1ObjectMeta(name=job_name), spec=spec)
 
     @staticmethod
-    def create_job(api_instance: BatchV1Api, job: V1Job):
+    def create_job(api_instance: BatchV1Api, job: V1Job) -> V1Job:
         api_response = api_instance.create_namespaced_job(body=job, namespace='default')
         logger.info(f"Job created: status={api_response.status}")
+        return api_response
 
     @staticmethod
-    def get_job_status(api_instance: BatchV1Api, job_name: str):
+    def get_job_status(api_instance: BatchV1Api, job_name: str) -> V1Job:
         job_completed = False
         while not job_completed:
-            api_response = api_instance.read_namespaced_job_status(name=job_name, namespace="default")
-            if api_response.status.succeeded is not None or api_response.status.failed is not None:
+            response = api_instance.read_namespaced_job_status(name=job_name, namespace="default")
+            if response.status.succeeded is not None or response.status.failed is not None:
                 job_completed = True
             time.sleep(1)
-            logger.info(f"Job status: status={api_response.status}")
+            logger.info(f"Job status: status={response.status}")
+        return response
 
     @staticmethod
-    def delete_job(api_instance: BatchV1Api, job_name: str):
+    def delete_job(api_instance: BatchV1Api, job_name: str) -> V1Status:
         api_response = api_instance.delete_namespaced_job(name=job_name, namespace='default',
                                                           body=client.V1DeleteOptions(propagation_policy='Foreground',
                                                                                       grace_period_seconds=0))
         logger.info(f"Job deleted: status={api_response.status}")
+        return api_response
+
+    # TODO: Add args in V1Container
+    # TODO: Mock Kubernetes in unit tests
